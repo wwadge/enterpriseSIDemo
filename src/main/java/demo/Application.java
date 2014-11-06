b7793ae06c7590338a288695b83a50deba609d2b
Wallace Wadge <wwadge@gmail.com>
2014-11-06 20:47:53 +0100

    Executor channels for seperate thread invocation

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.channel.PublishSubscribeChannel;
+import org.springframework.integration.channel.ExecutorChannel;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.SubscribableChannel;
 import org.springframework.messaging.support.MessageBuilder;
+
+import java.util.concurrent.Executors;
 
 @Configuration
 @ComponentScan
@@ -23,12 +25,13 @@ public class Application {
 
 
         // 1-to-n, same thread
-        SubscribableChannel channel = new PublishSubscribeChannel();
+        SubscribableChannel channel = new ExecutorChannel(Executors.newSingleThreadExecutor());
 
         // somewhere else....
-        channel.subscribe(message -> log.debug(message.toString()));
-        channel.subscribe(message -> log.debug("Hey there, I'm also called"));
+        channel.subscribe(message -> log.debug(Thread.currentThread() + " : " + message.toString()));
+        channel.subscribe(message -> log.debug(Thread.currentThread() + ": Hey there, I'm also called"));
 
+        log.debug("Sender thread ID : " + Thread.currentThread());
         channel.send(msg);
 
 //        SpringApplication.run(Application.class, args);

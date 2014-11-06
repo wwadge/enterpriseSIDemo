43260ff8c260f302b55d4ead2f9118379875cc10
Wallace Wadge <wwadge@gmail.com>
2014-11-06 20:28:04 +0100

    Send and receive basic

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
 @Configuration
@@ -18,6 +20,17 @@ public class Application {
         // Manual message construction
         Message<String> msg =  MessageBuilder.withPayload("Hello World!").setHeader("Name", "Wallace").build();
         log.debug(msg.toString());
+
+
+        PollableChannel channel = new QueueChannel();
+        channel.send(msg);
+
+        // somewhere else....
+        Message<?> msg2 = channel.receive();
+        log.debug(msg2.toString());
+
+
+
 
 //        SpringApplication.run(Application.class, args);
     }

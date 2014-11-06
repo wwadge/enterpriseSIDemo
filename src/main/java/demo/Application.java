3cbfc74d2940f86255983a6c86d35f4f7ea69874
Wallace Wadge <wwadge@gmail.com>
2014-11-06 20:14:34 +0100

    Manual message construction

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,16 +1,24 @@
 package demo;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@Slf4j
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        // Manual message construction
+        Message<String> msg =  MessageBuilder.withPayload("Hello World!").setHeader("Name", "Wallace").build();
+        log.debug(msg.toString());
+
+//        SpringApplication.run(Application.class, args);
     }
 }
